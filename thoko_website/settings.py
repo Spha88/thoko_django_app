22ea445f0efdd9f31c6bf2309823550b47dad901
SIPHAMANDLA MEHLOMAKULU <mr.smehlomakulu@gmail.com>
2022-11-18 09:49:49 +0200

    Add postgres database settings for local development

--- thoko_website/settings.py
+++ thoko_website/settings.py
@@ -64,10 +64,20 @@ WSGI_APPLICATION = 'thoko_website.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'tmtiki',
+        'USER': 'postgres',
+        'PASSWORD': 'Spha588.',
+        'HOST': 'localhost'
     }
 }
 
