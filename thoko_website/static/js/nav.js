0605c265950819c6a102868d5bab089f495e8c3e
SIPHAMANDLA MEHLOMAKULU <mr.smehlomakulu@gmail.com>
2022-12-15 05:26:24 +0200

    Refactor nav bar css to avoid horizontal scroll when nav is open

--- thoko_website/static/js/nav.js
+++ thoko_website/static/js/nav.js
@@ -1,7 +1,6 @@
-
 // Add or Remove the "open" class on nav list container on mobile devices
 let nav_button = document.getElementById("mobile-nav-button")
-let nav_items_container = document.getElementById("nav-items-container")
+let nav_items_container = document.getElementById("nav-items")
 
 nav_button.addEventListener("click", e => {
     let open = nav_items_container.classList.contains("open")
